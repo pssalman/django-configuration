6c1aa0fc12589c3e653d44a9c0936b792efcb2eb
Tony Salman <anton.salman@gmail.com>
2019-11-23 19:23:43 +0200

    fixing environment variable to load settings file

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          f"conf.settings.{os.environ.get('ENV', 'development')}")
+                          f"conf.settings.{os.environ.get('ENVIRONMENT')}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
